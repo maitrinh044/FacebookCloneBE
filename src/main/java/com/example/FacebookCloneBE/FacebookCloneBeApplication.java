8a284bad3094af6cf644b0b29b3aa9dec15fd5f4
maitrinh044 <maitrinh22112004@gmail.com>
2025-03-18 17:44:43 +0700

    Initial commit

---
+++ src/main/java/com/example/FacebookCloneBE/FacebookCloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.FacebookCloneBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookCloneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookCloneBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FacebookCloneBE/FacebookCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FacebookCloneBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookCloneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
