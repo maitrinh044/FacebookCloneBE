c9e284ca593811d06fb85ef54c8156a35cc6d894
maitrinh044 <maitrinh22112004@gmail.com>
2025-03-18 18:09:10 +0700

    add user controller, user repository

--- src/main/java/com/example/FacebookCloneBE/Controller/UserController.java
+++ src/main/java/com/example/FacebookCloneBE/Controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.FacebookCloneBE.Controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
 
 }
--- src/main/java/com/example/FacebookCloneBE/Repository/UserRepository.java
+++ src/main/java/com/example/FacebookCloneBE/Repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.FacebookCloneBE.Repository;
 
-public interface UserRepository {
+import com.example.FacebookCloneBE.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
