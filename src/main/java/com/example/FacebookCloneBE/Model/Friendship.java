df6964755ea123cdd580ab3c5493dea60de1b4ed
maitrinh044 <maitrinh22112004@gmail.com>
2025-03-19 08:51:44 +0700

    add friendship, message, notification model & repo

---
+++ src/main/java/com/example/FacebookCloneBE/Model/Friendship.java
@@ -0,0 +1,28 @@
+package com.example.FacebookCloneBE.Model;
+
+import com.example.FacebookCloneBE.Enum.FriendshipTypeEnum;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "friend_ship")
+public class Friendship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "user_1")
+    private User user1;
+    @ManyToOne
+    @JoinColumn(name = "user_2")
+    private User user2;
+    @Column
+    private FriendshipTypeEnum type;
+    @Column
+    private Timestamp createdAt;
+}
---
+++ src/main/java/com/example/FacebookCloneBE/Model/Notification.java
@@ -0,0 +1,29 @@
+package com.example.FacebookCloneBE.Model;
+
+import com.example.FacebookCloneBE.Enum.NotificationTypeEnum;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "notification")
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+    @Column
+    private String content;
+    @Column
+    private NotificationTypeEnum type;
+    @Column
+    private boolean isRead;
+    @Column
+    private Timestamp createdAt;
+}
