11ffacc7edb168bb921032258f8d936c1f5998ee
maitrinh044 <maitrinh22112004@gmail.com>
2025-03-20 09:14:52 +0700

    Merge branch 'hung'

---
+++ src/main/java/com/example/FacebookCloneBE/Repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.example.FacebookCloneBE.Repository;
+
+import com.example.FacebookCloneBE.Model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, String> {
+
+}
