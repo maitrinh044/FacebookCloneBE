4e514e4371e3a27b03c0d594dbf597a3ce18e986
thankhanh <hathanhkhanh55204@gmail.com>
2025-03-20 08:37:10 +0700

    khanh update Post, Comment, Like

--- src/main/java/com/example/FacebookCloneBE/Repository/LikeRepository.java
+++ src/main/java/com/example/FacebookCloneBE/Repository/LikeRepository.java
@@ -1,9 +1,12 @@
 package com.example.FacebookCloneBE.Repository;
 
-import com.example.FacebookCloneBE.Model.ChatGroup;
+import com.example.FacebookCloneBE.Model.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ChatGroupRepository extends JpaRepository<ChatGroup, Long> {
+public interface LikeRepository extends JpaRepository<Like, String> {
+
 }
