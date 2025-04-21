c0165910477c9aa8314a5da486794e5b1f753ad2
maitrinh044 <maitrinh22112004@gmail.com>
2025-04-21 23:17:15 +0700

    Update PostController.java

--- src/main/java/com/example/FacebookCloneBE/Controller/PostController.java
+++ src/main/java/com/example/FacebookCloneBE/Controller/PostController.java
@@ -5,22 +5,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.FacebookCloneBE.DTO.PostDTO.PostDTO;
 import com.example.FacebookCloneBE.Reponse.ResponseData;
 import com.example.FacebookCloneBE.Service.PostService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/posts")
 public class PostController {
 
     @Autowired
